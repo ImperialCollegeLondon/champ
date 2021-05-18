272f422ae74e8433e3b0850d99741efae996079e
Christopher Cave-Ayland <c.cave-ayland@imperial.ac.uk>
2021-05-18 17:13:37 +0100

    Update passenger_wsgi.py to use deployment venv

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,3 +1,11 @@
-import portal.wsgi
+import os
+import sys
+
+# swap Python interpreter to provided venv
+INTERP = os.environ["PORTAL_VENV"]
+if sys.executable != INTERP:
+    os.execl(INTERP, INTERP, *sys.argv)
+
+import portal.wsgi  # noqa: E402
 
 application = portal.wsgi.application
