ef65377ec22f263fa94d5bfb69523abd81a6ab62
Christopher Cave-Ayland <c.cave-ayland@imperial.ac.uk>
2021-05-20 17:01:02 +0100

    Update STATIC_URL for compatibility with OOD server

--- portal/settings/settings.py
+++ portal/settings/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "/public/portal/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
