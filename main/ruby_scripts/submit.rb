99fb532b6d05eda82d94b922ebbccd1827f51727
Christopher Cave-Ayland <c.cave-ayland@imperial.ac.uk>
2021-07-12 22:23:03 +0100

    Place job stdout/stderr into separate named files

--- main/ruby_scripts/submit.rb
+++ main/ruby_scripts/submit.rb
@@ -8,5 +8,7 @@ cluster = clusters["rcs"]
 script = OodCore::Job::Script.new(
   content: Pathname.new(script_path).read,
   accounting_id: nil,
+  output_path: "job_output",
+  error_path: "job_errors"
 )
 puts cluster.job_adapter.submit(script)
