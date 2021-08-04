09b34222b94101b81ba2f0b7a1259a06410e9c3b
Christopher Cave-Ayland <c.cave-ayland@imperial.ac.uk>
2021-08-04 10:09:00 +0100

    Add exclude filter for description

--- main/filters.py
+++ main/filters.py
@@ -9,5 +9,11 @@ class JobFilter(django_filters.FilterSet):
         fields = ("description", "project", "status", "resources", "software")
 
     description = django_filters.CharFilter(lookup_expr="icontains")
+    not_description = django_filters.CharFilter(
+        field_name="description",
+        lookup_expr="icontains",
+        exclude=True,
+        label="Description does not contain",
+    )
     resources = django_filters.AllValuesFilter()
     software = django_filters.AllValuesFilter()
