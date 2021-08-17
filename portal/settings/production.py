72f36a3983966c10dfb5ba9086dd69f268082ba1
Christopher Cave-Ayland <c.cave-ayland@imperial.ac.uk>
2021-08-17 10:38:53 +0100

    Update production.py to support site specific deployments

--- portal/settings/production.py
+++ portal/settings/production.py
@@ -1,15 +1,17 @@
+"""This module contains those settings required for production
+deployment as an Open OnDemand app that are not expected to vary
+between deployments. This module is not intended to be used directly
+but imported into another module where deployment specific settings
+are given.
+"""
+
 import os
 from pathlib import Path
 
 from .settings import *  # noqa: F401, F403
 
 DEBUG = False
-ALLOWED_HOSTS = ["openondemand.rcs.ic.ac.uk"]
 SECRET_KEY = os.environ["SECRET_KEY"]
-EMAIL_HOST = "smarthost.cc.ic.ac.uk"
-SERVER_EMAIL = "noreply@imperial.ac.uk"
-DEFAULT_FROM_EMAIL = "noreply@imperial.ac.uk"
-ADMINS = [("Christopher Cave-Ayland", "c.cave-ayland@imperial.ac.uk")]
 MIDDLEWARE.insert(1, "whitenoise.middleware.WhiteNoiseMiddleware")  # noqa: F405
 SECURE_BROWSER_XSS_FILTER = True
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
@@ -17,6 +19,9 @@ SECURE_HSTS_SECONDS = 15552000
 USE_X_FORWARDED_HOST = True
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
+
+# The below settings are sensible defaults but may nonetheless need
+# to be overridden for different sites
 DATABASES["default"]["NAME"] = (  # noqa: F405
     Path(os.environ["HOME"]) / "chemistry_portal_db_DO_NOT_DELETE.sqlite3"
 )
